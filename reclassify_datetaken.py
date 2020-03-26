4299c935f665e0f6a90636251037d09f8259aaf6
dcf1007 <dcf1007@gmail.com>
2020-03-26 12:13:41 +0100

    Implement date extraction from EXIF
    The date of the pictures will be extracted from the EXIF metadata. In case that the fle doesn not have, the last modified date will be used (for example in the case of videos)

--- reclassify_datetaken.py
+++ reclassify_datetaken.py
@@ -2,11 +2,31 @@ import glob
 import os
 import shutil
 import pathlib
+import PIL.Image
+from datetime import datetime
 
 directory = pathlib.Path("./example_files")
+
+def get_date(filename):
+	#The EXIF index to the DateOriginal code is 36867 (0x9003)
+	#The EXIF time is always in the following format: "YYYY:MM:DD HH:MM:SS" 
+	with PIL.Image.open(filename) as image:
+		image.verify()
+		try:
+			return datetime.strptime(image._getexif()[36867], "%Y:%m:%d %H:%M:%S")
+		except:
+			return datetime.fromtimestamp(os.path.getmtime(filename))
+
 for File in directory.iterdir():
 	if os.path.isfile(File):
-		print(File)
-	else:
-		print(str(File) + " <--- Folder: ignored")
+		FileDate = get_date(File)
+		print(FileDate.strftime("%Y-%m-%d %H:%M:%S") + "\t" + str(File.name))
+		NewFolder = str(File.parent) + os.path.sep + FileDate.strftime("%Y-%m-%d")
+		if os.path.exists(NewFolder) == False:
+			os.mkdir(NewFolder)
+			shutil.move(File, NewFolder + os.path.sep + str(File.name))
+		elif os.path.isfile(NewFolder) == True:
+				print(FileDate.strftime("%Y-%m-%d") + " exists as a file. Not moving " + str(File.name))
+		else:
+			shutil.move(File, NewFolder + os.path.sep + str(File.name))
 input("Press any key to exit")
