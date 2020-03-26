b2c2e1c8cc78e23e036c68d0cf7d4877c6111375
dcf1007 <dcf1007@gmail.com>
2020-03-26 13:14:20 +0100

    Implemented a method to find the most reliable date for all files with the same stem
    The script assumes that 2 files with the same stem belong together

--- reclassify_datetaken.py
+++ reclassify_datetaken.py
@@ -8,27 +8,46 @@ from datetime import datetime
 directory = pathlib.Path(input("Please write(or drag) the path to process: "))
 
 def get_date(filename):
+	'''
+	The method returns a tuple (isEXIF, date) where the first value indicates 
+	if the date is from EXIF or from the OS and the second contains the date itself
+	'''
 	#The EXIF index to the DateOriginal code is 36867 (0x9003)
 	#The EXIF time is always in the following format: "YYYY:MM:DD HH:MM:SS" 
 	try:
 		with PIL.Image.open(filename) as image:
 			image.verify()
-			return datetime.strptime(image._getexif()[36867], "%Y:%m:%d %H:%M:%S")
+			return (1, datetime.strptime(image._getexif()[36867], "%Y:%m:%d %H:%M:%S"))
 	except:
-		return datetime.fromtimestamp(os.path.getmtime(filename))
+		return (0, datetime.fromtimestamp(os.path.getmtime(filename)))
 
 for File in directory.iterdir():
-	if os.path.isfile(File) and str(File.suffix) != ".xmp":
-		FileDate = get_date(File)
-		print(FileDate.strftime("%Y-%m-%d %H:%M:%S") + "\t" + str(File.name))
-		NewFolder = str(File.parent) + os.path.sep + FileDate.strftime("%Y-%m-%d")
+	if os.path.isfile(File):
+		FileDate = None
+		#Get all the files with the same stem
+		SameStemFiles = File.parent.glob(str(File.stem) + ".*")
+		for SameStemFile in SameStemFiles:
+			if FileDate:
+				SameStemFileDate = get_date(SameStemFile)
+				#If both dates come from the same source (either OS or EXIF) and
+				#If the new file has an older date than the one stored
+				if FileDate[0] == SameStemFileDate[0] and FileDate[1] > SameStemFileDate[1]:
+					FileDate = SameStemFileDate #Replace the stored date with the new one
+				#If the date stored is from the OS and the new one is from EXIF
+				elif FileDate[0] < SameStemFileDate[0]:
+					FileDate = SameStemFileDate #Replace the stored date with the new one
+			else:
+				FileDate = get_date(SameStemFile)
+		
+		print(FileDate[1].strftime("%Y-%m-%d %H:%M:%S") + "\t" + str(File.name))
+		NewFolder = str(File.parent) + os.path.sep + FileDate[1].strftime("%Y-%m-%d")
 		if os.path.exists(NewFolder) == False:
 			os.mkdir(NewFolder)
 			shutil.move(File, NewFolder + os.path.sep + str(File.name))
 			if os.path.exists(str(File.parent) + os.path.sep + str(File.stem) + ".xmp"):
 				shutil.move(str(File.parent) + os.path.sep + str(File.stem) + ".xmp", NewFolder + os.path.sep + str(File.stem) + ".xmp")
 		elif os.path.isfile(NewFolder) == True:
-				print(FileDate.strftime("%Y-%m-%d") + " exists as a file. Not moving " + str(File.name))
+				print(FileDate[1].strftime("%Y-%m-%d") + " exists as a file. Not moving " + str(File.name))
 		else:
 			shutil.move(File, NewFolder + os.path.sep + str(File.name))
 			if os.path.exists(str(File.parent) + os.path.sep + str(File.stem) + ".xmp"):
